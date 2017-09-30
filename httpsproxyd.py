d10643ee6eab07e618bb671f2a5c9f34fc8fe0f1
Tomasz 'Trog' Welman <tomasz.w@yggdrasilgaming.com>
2017-10-01 01:01:33 +0200

    code styling changes

--- httpsproxyd.py
+++ httpsproxyd.py
@@ -1,16 +1,38 @@
 #!/usr/local/bin/python3
-
+import ssl
+import re
+import os
+import sys
+import signal
+import argparse
+import requests  # this dependency needs to be installed manually
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
-import requests, ssl, re
-#requests dependency needs to be installed manually
+from threading import Lock
 
-HOST = '0.0.0.0'
-PORT = 4443
-SERVER_CERTFILE_PATH = './server.pem' # has to contain private key, first the cert, then the key
-ALLOWED_URL_REGEXPS = [
-    re.compile('https://static[a-z0-9_-]+\.yggdrasilgaming\.com',re.I),
+HSPD_EXIT_SUCCESS = 0
+HSPD_EXIT_FAILURE = 1
+HSPD_HOST = '0.0.0.0'
+HSPD_PORT = 4443
+HSPD_SERVER_CERTFILE_PATH = './server.pem'  # has to contain private key, first the cert, then the key
+HSPD_LOG_FILE = '/var/log/httpsproxyd/httpsproxyd.log'
+HSPD_PID_FILE = '/var/run/httpsproxyd.pid'
+HSPD_ALLOWED_URL_REGEXPS = [
+    re.compile('https://someurl[a-z0-9_-]+\.example\.com', re.I),
 ]
+hspd_stderr_lock = Lock()
+hspd_stderr = sys.stderr
+hspd_pid_file = HSPD_PID_FILE
+
+
+def log(log_item):
+    with hspd_stderr_lock:
+        try:
+            hspd_stderr.write(str(log_item) + '\n')
+            hspd_stderr.flush()
+        except Exception:
+            pass
+
 
 class RequestHandler(BaseHTTPRequestHandler):
     def __init__(self, request, client_address, server):
@@ -22,7 +44,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def header_ok_to_rewrite(self, hdr):
         lower_key = hdr.lower()
-        return lower_key.startswith('accept') or lower_key in ['cache-control','user-agent']
+        return lower_key.startswith('accept') or lower_key in ['cache-control', 'user-agent']
 
     def prepare_headers(self, headers):
         hdrs = {}
@@ -37,20 +59,29 @@ class RequestHandler(BaseHTTPRequestHandler):
         else:
             self.send_error(resp.status_code)
         for key in resp.headers:
-            if key.lower() not in ['server','date','connection']:
+            if key.lower() not in ['server', 'date', 'connection']:
                 self.send_header(key, resp.headers[key])
         self.end_headers()
         self.wfile.write(resp.content)
 
     def url_is_allowed(self, url):
         if url is not None:
-            for url_regexp in ALLOWED_URL_REGEXPS:
+            for url_regexp in HSPD_ALLOWED_URL_REGEXPS:
                 if url_regexp.match(url) is not None:
                     return True
         return False
 
+    def log_message(self, format, *args):
+        log(format % args)
+
     def do_GET(self):
-        print(self.requestline)
+        try:
+            self.process_request()
+        except Exception as e:
+            log(e)
+
+    def process_request(self):
+        log(self.requestline)
         parsed = urlparse(self.path)
         params = parse_qs(parsed[4])
         if params is not None:
@@ -72,6 +103,86 @@ class RequestHandler(BaseHTTPRequestHandler):
         else:
             self.send_error(404)
 
-httpd = HTTPServer((HOST, PORT), RequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile=SERVER_CERTFILE_PATH, server_side=True)
-httpd.serve_forever()
+
+def daemonize():
+    pid = os.fork()
+    if pid > 0:
+        sys.exit(HSPD_EXIT_SUCCESS)
+    elif pid < 0:
+        log('fork failed: ' + str(pid))
+        sys.exit(HSPD_EXIT_FAILURE)
+    os.chdir('/')
+    os.setsid()
+    os.umask(0)
+    sys.stdin.close()
+    sys.stdout.close()
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Runs a daemon acting as HTTPS proxy.')
+    parser.add_argument('-c', dest='cert_path', nargs=1, default=[HSPD_SERVER_CERTFILE_PATH], help='absolute path to certificate file')
+    parser.add_argument('-l', dest='log_file', nargs=1, default=[HSPD_LOG_FILE], help='absolute path to log file')
+    parser.add_argument('-p', dest='pid_file', nargs=1, default=[HSPD_PID_FILE], help='absolute path to PID file')
+    args = parser.parse_args()
+    return (args.cert_path[0], args.log_file[0], args.pid_file[0])
+
+
+def create_log_file(log_file_path):
+    try:
+        with hspd_stderr_lock:
+            global hspd_stderr
+            hspd_stderr = open(log_file_path, 'a')
+            sys.stderr.close()
+        log('PID: ' + str(os.getpid()))
+    except Exception as e:
+        log('error opening log file: %s: %s' % (log_file_path, str(e)))
+
+
+def create_pid_file(pid_file_path):
+    try:
+        with open(pid_file_path, 'w') as pid_file:
+            pid_file.write(str(os.getpid()))
+            global hspd_pid_file
+            hspd_pid_file = pid_file_path
+    except Exception as e:
+        log('error opening PID file: %s: %s' % (pid_file_path, str(e)))
+
+
+def remove_pid_file(pid_file_path):
+    try:
+        os.remove(pid_file_path)
+    except Exception as e:
+        log('error removing PID file: %s: %s' % (pid_file_path, str(e)))
+
+
+def exit_failure():
+    remove_pid_file(hspd_pid_file)
+    sys.exit(HSPD_EXIT_FAILURE)
+
+
+def handle_signal(signal_number, stack):
+    remove_pid_file(hspd_pid_file)
+    sys.exit(HSPD_EXIT_SUCCESS)
+
+
+if __name__ == '__main__':
+    x = (cert_file, log_file, pid_file) = parse_arguments()
+    daemonize()
+    signal.signal(signal.SIGINT, handle_signal)
+    signal.signal(signal.SIGTERM, handle_signal)
+    create_log_file(log_file)
+    create_pid_file(pid_file)
+    httpd = HTTPServer((HSPD_HOST, HSPD_PORT), RequestHandler)
+    try:
+        httpd.socket = ssl.wrap_socket(httpd.socket, certfile=cert_file, server_side=True)
+    except FileNotFoundError as e:
+        log('certificate error: ' + str(e))
+        exit_failure()
+    except ssl.SSLError as e:
+        log('certificate SSL error: ' + str(e))
+        exit_failure()
+    except Exception as e:
+        log('ssl.wrap_socket error: ' + str(e))
+        exit_failure()
+    httpd.serve_forever()
+    remove_pid_file(hspd_pid_file)
